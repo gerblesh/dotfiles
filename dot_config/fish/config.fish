7668a2feb0d136f469f9c46591a72cbc025c0156
gerblesh <101901964+gerblesh@users.noreply.github.com>
2024-10-20 12:46:39 -0700

    update

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -29,9 +29,14 @@ set -Ux XDG_DATA_DIRS "/home/linuxbrew/.linuxbrew/share:$XDG_DATA_DIRS"
         # Commands to run in interactive sessions can go here
         fish_vi_key_bindings
     end
-    function apply
+    function ba # brew apply
         chezmoi re-add
-        chezmoi apply -n
+        chezmoi apply
+    end
+    function cgitcom # chezmoi git commit
+        chezmoi re-add
+        chezmoi git add .
+        chezmoi git commit
     end
 
     # set fish_vi_force_cursor 1

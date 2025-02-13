eb6928b27691ae25dea437a6b37670e878239be9
gerblesh <101901964+gerblesh@users.noreply.github.com>
2025-02-13 10:25:42 -0800

    add more execs

--- dot_config/fish/conf.d/fish_prompt.fish
+++ dot_config/fish/conf.d/fish_prompt.fish
@@ -43,39 +43,6 @@ end
 
 ## end async
 
-function fish_mode_prompt
-    # set --local vi_mode_color
-    set --local vi_mode_symbol
-    switch $fish_bind_mode
-        case default
-            set vi_mode_symbol "N "
-        case insert
-            set vi_mode_symbol ""
-        case replace replace_one
-            set vi_mode_symbol "R "
-        case visual
-            set vi_mode_symbol "V "
-    end
-    echo -ns (set_color -od brmagenta) "$vi_mode_symbol" (set_color normal)
-end
-
-function fish_prompt
-    set -l last_status $pipestatus
-    set -l stat
-
-    for status_code in $last_status
-        if test "$status_code" != 0
-            set stat (set_color -i brred)" $last_status"(set_color normal)
-            break
-        end
-    end
-
-    # Silverblue workaround
-    set -l pwd (string replace /var/home /home $PWD)
-
-    echo -ns (set_color -o cyan)(prompt_pwd -D 1 $pwd) (set_color normal) "$stat "
-end
-
 set -g __fish_git_prompt_showupstream informative
 set -g __fish_git_prompt_showdirtystate true
 set -g __fish_git_prompt_showstashstate true
@@ -85,25 +52,3 @@ set -g __fish_git_prompt_char_upstream_behind " ↓"
 set -g __fish_git_prompt_char_upstream_ahead " ↑"
 set -g __fish_git_prompt_char_stagedstate "●"
 set -g __fish_git_prompt_char_untrackedfiles "!"
-
-function fish_right_prompt
-    set -l duration $CMD_DURATION
-    make_async_request
-
-    # greater than 10 seconds
-    # duration, not the most useful info tbh
-    # if test $duration -gt 10000
-    #     echo -ns (set_color -od brblack) "$duration" "ms "(set_color normal)
-    # end
-
-    if test -e /run/.containerenv
-        echo -ns (set_color -od brmagenta) " "(string match -rg 'name="(.*)"'</run/.containerenv)(set_color normal)
-    end
-
-    async_print_buffer
-
-    # Reset state, prompt is complete
-    if test "$state" -eq "$REPAINTING"
-        set state "$INITIAL"
-    end
-end

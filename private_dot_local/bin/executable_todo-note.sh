ce28162f1fe1ce55a8e1027931c8248b8c1aab7d
gerblesh <101901964+gerblesh@users.noreply.github.com>
2025-07-15 14:44:54 -0700

    fix: update all the old scripts to use floatterm-exec

--- private_dot_local/bin/executable_todo-note.sh
+++ private_dot_local/bin/executable_todo-note.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env fish
 
 
-ghostty --class="com.gar.todo" --background-opacity=1.0 --window-width=100 --window-height=30 --gtk-single-instance=false -e "/bin/fish '-ic hx $HOME/journal/todo.md'"
+floatterm-exec note todo

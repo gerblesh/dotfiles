8b148f59733e70f9ab356a3f683b717d627bd412
gerblesh <101901964+gerblesh@users.noreply.github.com>
2025-08-17 23:08:23 -0400

    update it all

--- dot_config/helix/helix.scm
+++ dot_config/helix/helix.scm
@@ -1,28 +1,34 @@
 (require "helix/configuration.scm")
-(require "cogs/keymaps.scm")
+(require (prefix-in helix. "helix/commands.scm"))
 (require "helix/ext.scm")
 (require (prefix-in helix.static. "helix/static.scm"))
 (require (prefix-in helix. "helix/commands.scm"))
+(require "modeline/modeline.scm")
+(require-builtin helix/core/text)
 
 ; Actual config
 (helix.theme "carbonfox_transparent")
 (inline-diagnostics-end-of-line-enable "hint")
 (inline-diagnostics-cursor-line-enable "warning")
 (cursor-shape #:normal 'block #:select 'underline #:insert 'bar)
-(line-number 'relative)
+
+;; requires PR: https://github.com/mattwparas/helix/pull/54
+; (line-number 'relative)
+; (whitespace (ws-visible #t))
+; (indent-guides (ig-render #t) (ig-character #\╎) (ig-skip-levels 1))
+; (jump-label-alphabet "jfkdls;aurieowpqnvmcxz")
 (mouse #f)
-(whitespace (ws-visible #t))
-(indent-guides (ig-render #t) (ig-character #\╎) (ig-skip-levels 1))
-(jump-label-alphabet "jfkdls;aurieowpqnvmcxz")
 (soft-wrap (sw-enable #t) (sw-max-wrap 25) (sw-max-indent-retain 0))
+(rainbow-brackets #t)
+(require "cogs/keymaps.scm")
 (keymap (global)
         (normal (ret "goto_word")
                 (L "move_next_sub_word_end")
                 (H "move_prev_sub_word_start")
                 (space (z ":sh zathura \"$(echo %{buffer_name} | sed 's/\\.[^.]*$/.pdf/')\"")
                        (i ":yank-diagnostic")
-                       (l ":o .github"))))
-(rainbow-brackets #t)
+                       (l ":o .github")
+                       (b ":sh git blame -L %{cursor_line},%{cursor_line} %{buffer_name}"))))
 
 ; LSP config
 (define-lsp "steel-language-server" (command "steel-language-server") (args '()))
@@ -32,16 +38,22 @@
                  (language-servers '("steel-language-server")))
 (define-language "c-sharp" (language-servers '("csharp-ls")))
 (define-language "python" (language-servers '("pyright")))
-(define-lsp "tinymist"
-            (command "tinymist")
-            (config (exportPdf "onType") (outputPath "$root/$dir/$name")))
+(define-lsp "tinymist" (config (exportPdf "onType") (outputPath "$root/$dir/$name")))
+(define-lsp "godot" (command "nc") (args '("127.0.0.1" "6005")))
+(define-language "gdscript" (language-servers '("godot")))
 
-; helper scheme functions from ext.scm
+;; Plugins
+(modeline-enable)
+(provide refresh-modeline)
+
+; config helpers
 (define (open-helix-scm)
   (helix.open (helix.static.get-helix-scm-path)))
 (define (open-init-scm)
   (helix.open (helix.static.get-init-scm-path)))
+; helper scheme functions from ext.scm
 (provide open-helix-scm
          open-init-scm
          evalp
-         eval-buffer)
+         eval-buffer
+         rope-regex)
